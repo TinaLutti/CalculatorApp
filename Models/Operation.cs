2c5d0c879ee79b0b9ed157a8cdb560dc8abc6715
TinaLutti <143405689+TinaLutti@users.noreply.github.com>
2025-08-21 10:51:34 +0200

    Initial commit

---
+++ Models/Operation.cs
@@ -0,0 +1,14 @@
+﻿namespace CalculatorApp.Models
+{
+    public class Operation
+    {
+        public double NumberA { get; set; }
+        public double NumberB { get; set; }
+        public double Result { get; set; }
+
+        public enum OperationType { Addition, Subtraction, Multiplication, Division }
+        public OperationType SelectedOperation { get; set; }
+
+
+    }
+}
