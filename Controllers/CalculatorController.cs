004ee8ef6adb2acb526690945a19fea90016aa42
TinaLutti <143405689+TinaLutti@users.noreply.github.com>
2025-08-21 16:40:03 +0200

    added CalculatorController, and simple html for testing purposes

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,49 @@
+﻿using CalculatorApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorApp.Controllers;
+
+public class CalculatorController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+
+[HttpPost]
+public IActionResult Index(Operation model)
+    {
+        if (ModelState.IsValid)
+        {
+            switch (model.SelectedOperation)
+            {
+                case Operation.OperationType.Addition:
+                    model.Result = model.Operand1 + model.Operand2;
+                    break;
+
+                case Operation.OperationType.Subtraction:
+                    model.Result = model.Operand1 - model.Operand2;
+                 break;
+
+                case Operation.OperationType.Multiplication:
+                    model.Result = model.Operand1 * model.Operand2;
+                    break;
+
+                case Operation.OperationType.Division:
+                    if (model.Operand2 == 0)
+                    {
+                        ModelState.AddModelError(nameof(model.Operand2), "It´s not possible to divide 0");
+                    }
+                    else
+                    {
+                        model.Result = model.Operand1 / model.Operand2;                       
+                    }
+                    break;
+
+            }
+        }
+        return View(model);
+    }
+}
--- Models/Operation.cs
+++ Models/Operation.cs
@@ -2,13 +2,13 @@
 {
     public class Operation
     {
-        public double NumberA { get; set; }
-        public double NumberB { get; set; }
+        public double Operand1 { get; set; }
+        public double Operand2 { get; set; }
         public double Result { get; set; }
 
         public enum OperationType { Addition, Subtraction, Multiplication, Division }
         public OperationType SelectedOperation { get; set; }
 
-
+        /*public List<double> Operands { get; set; } = new List<double>(); Change Operand to List if implementing additional operands */
     }
 }
